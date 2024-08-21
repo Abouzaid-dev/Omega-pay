5e20b1cfc5f31a344264e632863bba0dde2f3729
Mahmoud Abouzaid <m.abouzaid.flutter.dev@gmail.com>
2024-08-21 03:33:12 +0300

    Onboarding Screen

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:omega_pay/home_page.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -8,8 +11,359 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  PageController nextpage = PageController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: PageView(
+        controller: nextpage,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 240,
+              ),
+              Image.asset(
+                "assets/1.png",
+                height: 266,
+                width: 186,
+                fit: BoxFit.fill,
+              ),
+              Text(
+                'Omega Pay',
+                style: TextStyle(
+                    fontFamily: 'Roboto Bold',
+                    fontSize: 50,
+                    shadows: [
+                      Shadow(
+                          color: Colors.black,
+                          blurRadius: 6,
+                          offset: Offset(2, 2))
+                    ]),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.circle_rounded, size: 20),
+                      Icon(Icons.circle_outlined, size: 20),
+                      Icon(Icons.circle_outlined, size: 20),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          nextpage.animateToPage(1,
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeOut);
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 70,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Color(0xff29C16E),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Text(
+                            ">",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontFamily: 'Roboto Regular'),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 200,
+              ),
+              Image.asset(
+                "assets/2.png",
+                height: 300,
+                width: 420,
+                fit: BoxFit.fill,
+              ),
+              Text(
+                'Fast & Secure',
+                style: TextStyle(
+                    fontFamily: 'Roboto Regular',
+                    fontSize: 40,
+                    shadows: [
+                      Shadow(
+                          color: Colors.black,
+                          blurRadius: 6,
+                          offset: Offset(2, 2))
+                    ]),
+              ),
+              Text(
+                'Payments',
+                style: TextStyle(
+                    fontFamily: 'Roboto Bold',
+                    fontSize: 50,
+                    shadows: [
+                      Shadow(
+                          color: Colors.black,
+                          blurRadius: 6,
+                          offset: Offset(2, 2))
+                    ]),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.circle_outlined, size: 20),
+                      Icon(Icons.circle_rounded, size: 20),
+                      Icon(Icons.circle_outlined, size: 20),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          nextpage.animateToPage(0,
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeOut);
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 229, 222, 222),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Text(
+                            "<",
+                            style: TextStyle(
+                                fontSize: 35, color: Color(0xff29C16E)),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          nextpage.animateToPage(2,
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeOut);
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 160,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Color(0xff29C16E),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          child: Text(
+                            "Next       >",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: 'Roboto Bold'),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 200,
+              ),
+              Image.asset(
+                "assets/3.png",
+                height: 300,
+                width: 420,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Send money to\nanyone at anytime.!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'Roboto Bold',
+                    fontSize: 40,
+                    shadows: [
+                      Shadow(
+                          color: Colors.black,
+                          blurRadius: 6,
+                          offset: Offset(2, 2))
+                    ]),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.circle_outlined, size: 20),
+                      Icon(Icons.circle_outlined, size: 20),
+                      Icon(Icons.circle_rounded, size: 20),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          nextpage.animateToPage(1,
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeOut);
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 229, 222, 222),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Text(
+                            "<",
+                            style: TextStyle(
+                                fontSize: 35, color: Color(0xff29C16E)),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          nextpage.animateToPage(3,
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeOut);
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 160,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Color(0xff29C16E),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          child: Text(
+                            "Next       >",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: 'Roboto Bold'),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 200,
+              ),
+              Image.asset(
+                "assets/4.png",
+                height: 300,
+                width: 420,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Get discounts\non every transactions',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'Roboto Bold',
+                    fontSize: 40,
+                    shadows: [
+                      Shadow(
+                          color: Colors.black,
+                          blurRadius: 6,
+                          offset: Offset(2, 2))
+                    ]),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomePage()),
+                          );
+                        },
+                        autofocus: true,
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          width: 400,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Color(0xff29C16E),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text(
+                            "Get Started",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontFamily: 'Roboto Bold'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:omega_pay/home_page.dart';
-// import 'package:lottie/lottie.dart';
+import 'package:omega_pay/onboarding_screen.dart';
 
 
 class SplashScreen extends StatefulWidget {
@@ -18,14 +18,14 @@ class _SplashScreenState extends State<SplashScreen> {
       body: FlutterSplashScreen.fadeIn(
         useImmersiveMode: true,
         duration: const Duration(seconds: 10),
-        nextScreen: const HomePage(),
+        nextScreen: const OnboardingScreen(),
         backgroundColor: Colors.white,
         childWidget: SizedBox(
           child: Center(
             child: Image.asset(
               'assets/1.png',
-              width: 400,
-              height: 500,
+              width: 200,
+              height: 200,
               fit: BoxFit.contain,
             ),
           ),
